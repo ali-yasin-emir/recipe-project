fbbffe2e7954980f1f524c67aad500d849f0ea4d
Ali Yasin Emir <aliyasin.emir16@gmail.com>
2022-11-11 04:46:25 +0300

    all files have been changed for refactoring

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,5 +1,5 @@
 import * as model from './model.js';
-import icons from 'url:../img/icons.svg';
+import recipeView from './views/recipeView.js';
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
@@ -13,123 +13,21 @@ const timeout = function (s) {
   });
 };
 
-const renderSpinner = function (parentEl) {
-  const markup = `
-  <div class="spinner">
-  <svg>
-    <use href="${icons}#icon-loader"></use>
-  </svg>
-</div>
-`;
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML('afterbegin', markup);
-};
-
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
     console.log(id);
 
     if (!id) return;
-    renderSpinner(recipeContainer);
+    recipeView.renderSpinner();
 
     // 1) Loading recipe
     await model.loadRecipe(id);
-    const { recipe } = model.state.recipe;
+
     // 2) Rendering recipe
-    const markup = `
-    <figure class="recipe__fig">
-    <img src="${recipe.image}" alt="${recipe.image}" class="recipe__img" />
-    <h1 class="recipe__title">
-      <span>${recipe.title}</span>
-    </h1>
-  </figure>
-  
-  <div class="recipe__details">
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="src/img/icons.svg#icon-clock"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--minutes">45</span>
-      <span class="recipe__info-text">minutes</span>
-    </div>
-    <div class="recipe__info">
-      <svg class="recipe__info-icon">
-        <use href="${icons}#icon-users"></use>
-      </svg>
-      <span class="recipe__info-data recipe__info-data--people">4</span>
-      <span class="recipe__info-text">servings</span>
+    recipeView.render(model.state.recipe);
 
-      <div class="recipe__info-buttons">
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="${icons}.svg#icon-minus-circle"></use>
-          </svg>
-        </button>
-        <button class="btn--tiny btn--increase-servings">
-          <svg>
-            <use href="${icons}.svg#icon-plus-circle"></use>
-          </svg>
-        </button>
-      </div>
-    </div>
-
-    <div class="recipe__user-generated">
-      <svg>
-        <use href="${icons}.svg#icon-user"></use>
-      </svg>
-    </div>
-    <button class="btn--round">
-      <svg class="">
-        <use href="${icons}.svg#icon-bookmark-fill"></use>
-      </svg>
-    </button>
-  </div>
-
-  <div class="recipe__ingredients">
-    <h2 class="heading--2">Recipe ingredients</h2>
-    <ul class="recipe__ingredient-list">
-    ${recipe.ingredients
-      .map(ing => {
-        return `
-    <li class="recipe__ingredient">
-    <svg class="recipe__icon">
-      <use href="${icons}.svg#icon-check"></use>
-    </svg>
-    <div class="recipe__quantity">${ing.quantity}</div>
-    <div class="recipe__description">
-      <span class="recipe__unit">${ing.unit}</span>
-    ${ing.description}
-      </div>
-  </li>
-    `;
-      })
-      .join('')}
-
-
-  <div class="recipe__directions">
-    <h2 class="heading--2">How to cook it</h2>
-    <p class="recipe__directions-text">
-      This recipe was carefully designed and tested by
-      <span class="recipe__publisher">${
-        recipe.publisher
-      }</span>. Please check out
-      directions at their website.
-    </p>
-    <a
-      class="btn--small recipe__btn"
-      href="${recipe.sourceUrl}"
-      target="_blank"
-    >
-      <span>Directions</span>
-      <svg class="search__icon">
-        <use href="${icons}.svg#icon-arrow-right"></use>
-      </svg>
-    </a>
-  </div>
-    `;
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', markup);
+    const recipeView = new RecipeView(model.state.recipe);
   } catch (err) {
     alert(err);
   }
