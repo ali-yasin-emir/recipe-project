636eeae415c06d53949862855e78ec0bc6ad322c
Ali Yasin Emir <aliyasin.emir16@gmail.com>
2022-11-12 06:19:06 +0300

    search results and errors have been prepared

--- controller.js
+++ controller.js
@@ -2,8 +2,13 @@ import * as model from './model.js';
 import 'core-js/stable';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
 // const recipeContainer = document.querySelector('.recipe');
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 const controlRecipes = async function () {
   try {
@@ -24,6 +29,8 @@ const controlRecipes = async function () {
 
 const controlSearchResults = async function () {
   try {
+    resultsView.renderSpinner();
+
     // 1) Get search query
     const query = searchView.getQuery();
     if (!query) return;
@@ -32,7 +39,7 @@ const controlSearchResults = async function () {
     await model.loadSearchResults('query');
 
     // 3) Render results
-    console.log(model.state.search.results);
+    resultsView.render(model.state.search.results);
   } catch (err) {
     console.log(err);
   }
--- views/recipeView.js
+++ views/recipeView.js
@@ -1,75 +1,22 @@
 import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
+import View from './View.js';
 
-
-class RecipeView {
-  #parentElement = document.querySelector('.recipe');
-  #data;
-  #errorMessage =  `We could not find that recipe. Please try another one!`
-  #message = '';
-
-  render(data) {
-    this.#data = data;
-    const markup = this.#generateMarkup();
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  #clear() {
-    this.#parentElement.innerHTML = '';
-  }
-
-  renderSpinner = function () {
-    const markup = `
-    <div class="spinner">
-    <svg>
-      <use href="${icons}#icon-loader"></use>
-    </svg>
-  </div>
-  `;
-    this.#clear() = '';
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  };
-
-  renderError(message = this.#errorMessage) {
-    const markup = `
-   <div class="error">
-      <div>
-        <svg>
-          <use href="${icons}#icon-alert-triangle"></use>
-        </svg>
-      </div>
-      <p>${message}</p>
-    </div>`;
-    this.#clear() = '';
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  renderMessage(message = this.#message) {
-    const markup = `
-   <div class="message">
-      <div>
-        <svg>
-          <use href="${icons}#icon-alert-triangle"></use>
-        </svg>
-      </div>
-      <p>${message}</p>
-    </div>`;
-    this.#clear() = '';
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
+class RecipeView extends View {
+  _parentElement = document.querySelector('.recipe');
+  _errorMessage = `We could not find that recipe. Please try another one!`;
+  _message = '';
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(ev => window.addEventListener(ev, handler));
   }
 
-  #generateMarkup() {
+  _generateMarkup() {
     return `
 <figure class="recipe__fig">
-<img src="${this.#data.image}" alt="${this.#data.image}" class="recipe__img" />
+<img src="${this._data.image}" alt="${this._data.image}" class="recipe__img" />
 <h1 class="recipe__title">
-  <span>${this.#data.title}</span>
+  <span>${this._data.title}</span>
 </h1>
 </figure>
 
@@ -101,9 +48,7 @@ class RecipeView {
   </div>
 </div>
 <div class="recipe__user-generated">
-  <svg>
-    <use href="${icons}.svg#icon-user"></use>
-  </svg>
+
 </div>
 <button class="btn--round">
   <svg class="">
@@ -114,7 +59,7 @@ class RecipeView {
 <div class="recipe__ingredients">
 <h2 class="heading--2">Recipe ingredients</h2>
 <ul class="recipe__ingredient-list">
-${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
+${this._data.ingredients.map(this._generateMarkupIngredient).join('')}
 
 <div class="recipe__directions">
 <h2 class="heading--2">How to cook it</h2>
@@ -137,7 +82,7 @@ ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
 `;
   }
 
-  #generateMarkupIngredient(ing) {
+  _generateMarkupIngredient(ing) {
     return `
 <li class="recipe__ingredient">
 <svg class="recipe__icon">
